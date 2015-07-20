598b849d07d011fc55c3e98786a4c2fe3474a6c5
islahul <islahulzunjani@gmail.com>
2015-07-20 20:12:40 +0530

    added dictionary to tree utility
    Signed-off-by: islahul <islahulzunjani@gmail.com>

---
+++ english-dictionary-mapper.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var _ = require('underscore');
+var dictionaryMap = {
+  dict: {}
+};
+
+fs.readFile(process.argv[2], 'utf8', function(err, txtFileData) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  var words = txtFileData.split('\n');
+
+  _.each(words, function(word) {
+    writeWordToDictionaryMap(dictionaryMap, word);
+  });
+
+  if(test(dictionaryMap)) {
+    writeDictionaryToFile('english-dictionary-tree.json', dictionaryMap.dict);
+  }
+});
+
+
+function writeWordToDictionaryMap(dictionaryMap, word) {
+  var pointer = dictionaryMap.dict;
+
+  _.each(word.split(''), function(character) {
+    character = character.toLowerCase();
+    pointer[character] = pointer[character] || {};
+    pointer = pointer[character];
+  });
+
+  pointer['.'] = true;
+}
+
+function writeDictionaryToFile(filename, dictionaryMap) {
+  fs.writeFile('./' + filename, JSON.stringify(dictionaryMap), function(err) {
+    if(err) {
+      console.log(err);
+      return;
+    }
+
+    console.log('Tree stored. Cheers!');
+  });
+}
+
+function test(dictionaryMap) {
+  return dictionaryMap.dict.h.e.l.l.o['.'];
+}
